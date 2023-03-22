8cbf29546a2b981c187e7c3f99ffe22ecc488909
Lau-na <luanetesta2@gmail.com>
2023-03-22 19:43:58 -0300

    Atividade finalizada

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+// import ScrollViewExample from './ScrollViewExample';
+// import FlatListExample from './FlatListExample';
+import SectionListExample from './SectionListExample';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <ScrollViewExample />
+    // <FlatListExample />
+    <SectionListExample />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
